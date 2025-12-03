3458917678eac5a8c5a2d23009f04ea7ad2e014b
Ilias <iliasaiakos@gmail.com>
2025-12-03 21:20:02 +0000

    second review

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class PasswordValidationException extends Exception {
 
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+
     public PasswordValidationException() {
         super("Wrong passwords");
     }
