2889ff203fd1cfdae9a085058c6bdc4620ba13e5
Ilias <iliasaiakos@gmail.com>
2025-12-03 21:02:12 +0000

    second review

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
-        throws PasswordValidationException {
-    if (password == null || repeatPassword == null) {
-        throw new PasswordValidationException();
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException();
         }
-    if (!password.equals(repeatPassword) || password.length() < 10) {
-        throw new PasswordValidationException();
+        if (!password.equals(repeatPassword) || password.length() < 10) {
+            throw new PasswordValidationException();
         }
     }
 }
